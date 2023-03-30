ae337554f637f14da91e7dc1178773a2c86001b7
ABU KALAM <99273096+Abukalam-web@users.noreply.github.com>
2023-03-30 07:54:59 +0600

    sddfjdkf

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+function nav () {
+const open = document.getElementById("n");
+const close = document.getElementById("nn");
+const nav1 = document.getElementById("nnn");
+
+open.addEventListener("click",function(){
+    nav1.style.display = "block";
+    close.style.display = "block";
+    open.style.display = "none";
+})
+
+close.addEventListener("click", function(){
+
+    nav1.style.display = "none";
+    close.style.display = "none";
+    open.style.display = "block";
+
+})
+
+}
+nav();
+
+
+
